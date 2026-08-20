25a341c3cbc6082ba0d5a81a150a72b416a8f7eb
walker117GG <paradaronald905@gmail.com>
2026-08-20 18:26:37 -0500

    primera versioon de la web

---
+++ script.js
@@ -0,0 +1,9 @@
+const boton = document.querySelector("#sonido")
+const audio = document.querySelector("#audiomamalon")
+
+boton.addEventListener(
+    "click", () =>{
+
+        audio.play()
+
+    });
